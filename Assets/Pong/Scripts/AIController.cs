12ada71fca97b93d851e485cbc01193bf86d02d0
Unknown <vazgriz@gmail.com>
2019-01-06 16:50:29 -0600

    Add AI paddle

---
+++ Assets/Pong/Scripts/AIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour {
+    [SerializeField]
+    GameObject ball;
+
+    Paddle paddle;
+    Rigidbody2D rb;
+    Rigidbody2D ballRB;
+
+    void Start() {
+        paddle = GetComponent<Paddle>();
+        rb = GetComponent<Rigidbody2D>();
+        ballRB = ball.GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        float paddleX = rb.position.x;
+        float ballX = ballRB.position.x;
+
+        paddle.Move(ballX - paddleX);
+    }
+}
