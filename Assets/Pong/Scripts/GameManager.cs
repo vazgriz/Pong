a820b865576c28d169ef9b4fc9212458c1ec8dc4
Unknown <vazgriz@gmail.com>
2019-01-05 21:46:19 -0600

    Add main menu

---
+++ Assets/Pong/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+    [SerializeField]
+    GameObject ui;
+
+    void Start() {
+        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(ui.gameObject);
+    }
+
+    public void LoadSinglePlayer() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMultiPlayer() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
