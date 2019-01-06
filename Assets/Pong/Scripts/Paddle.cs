b9bdfe111d0b5ad31b37ecd887e1a2eb57941bdf
Unknown <vazgriz@gmail.com>
2019-01-05 20:17:14 -0600

    Add Game scene

---
+++ Assets/Pong/Scripts/Paddle.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+    [SerializeField]
+    float speed = 1;
+    [SerializeField]
+    float acceleration = 1;
+    [SerializeField]
+    float extents = 0;
+
+    Rigidbody2D rb;
+    float moveDir;
+    float velocity;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(float dir) {
+        moveDir = dir;
+    }
+
+    void Update() {
+        if (Mathf.Abs(moveDir) > 0.1f) {
+            velocity = velocity + moveDir * acceleration * Time.deltaTime;
+        } else {
+            velocity = Mathf.Sign(velocity) * Mathf.Max(0, Mathf.Abs(velocity) - acceleration * Time.deltaTime);
+        }
+
+        velocity = Mathf.Clamp(velocity, -speed, speed);
+
+        float x = rb.position.x + velocity * Time.deltaTime;
+
+        if (x <= -extents) {
+            x = -extents;
+            velocity = 0;
+        } else if (x >= extents) {
+            x = extents;
+            velocity = 0;
+        }
+
+        rb.position = new Vector2(x, rb.position.y);
+    }
+}
---
+++ Assets/Pong/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField]
+    Paddle paddle;
+
+    public void Bind(Paddle paddle) {
+        this.paddle = paddle;
+    }
+
+    void Update() {
+        if (!paddle) return;
+
+        float input = 0;
+
+        if (Input.GetKey(KeyCode.A)) {
+            input -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            input += 1;
+        }
+
+        paddle.Move(input);
+    }
+}
